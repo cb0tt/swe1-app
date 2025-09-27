2455b05eda494c65ad2e57c1768645ce2dc8ecaf
cb0tt <cbottril23@stac.edu>
2025-09-27 15:33:17 -0400

    configs

--- web/settings.py
+++ web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$yv6^wr=^_^)l12ko5d1txg1anl@7ugk3=cg5aq$uldsicc9bz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangopolls.eba-usskqzin.us-east-2.elasticbeanstalk.com']
 
 
 # Application definition
