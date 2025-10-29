9b29140010b7b626f1111f831b8ef567600aea49
cb0tt <cbottril23@stac.edu>
2025-10-29 10:59:59 -0400

    Add coverage tracking, flake8 config, and CI workflow

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
